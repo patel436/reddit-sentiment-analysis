8d071e307fd6e85b525ad22d6648646999095ad9
Abhishek Singh <singh732@uwindsor.ca>
2023-07-23 23:28:05 -0400

    Registration Data Capture

--- reddit_sentiment_analyzer/admin.py
+++ reddit_sentiment_analyzer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Topic, Tweet
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Topic)
+admin.site.register(Tweet)
--- reddit_sentiment_analyzer/models.py
+++ reddit_sentiment_analyzer/models.py
@@ -10,6 +10,12 @@ class Tweet(models.Model):
 
     def __str__(self):
         return self.tweet_id
+    
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
 class Topic(models.Model):
     topic_name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField()
